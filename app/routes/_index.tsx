9b28d858701f0874b29ef4ca842067d16252f88f
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-29 15:45:04 -0300

    move layout around

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useState } from "react";
 import Logo from "~/assets/PokeMemLogo.png";
-import PichuSprite from "~/assets/pichuSprite.png";
-import PikachuSprite from "~/assets/pikachuSprite.png";
-import RaichuSprite from "~/assets/raichuSprite.png";
+import CharmanderSprite from "~/assets/char1.png";
+import CharmeleonSprite from "~/assets/char2.png";
+import CharizardSprite from "~/assets/char3.png";
 import SelectDifficulty from "~/components/SelectDifficulty";
 
 export const meta: MetaFunction = () => {
@@ -14,7 +15,7 @@ export const meta: MetaFunction = () => {
 
 export type GameDifficulty = {
   difficulty: string;
-  sprite: string;
+  sprite?: string;
   cardsPerTurn: number;
   cardsPerDeck: number;
 };
@@ -22,42 +23,51 @@ export type GameDifficulty = {
 export default function Index() {
   const difficulties: GameDifficulty[] = [
     {
-      difficulty: "Fácil",
-      sprite: PichuSprite,
+      difficulty: "Easy",
+      sprite: CharmanderSprite,
       cardsPerTurn: 4,
       cardsPerDeck: 20,
     },
     {
       difficulty: "Normal",
-      sprite: PikachuSprite,
+      sprite: CharmeleonSprite,
       cardsPerTurn: 6,
       cardsPerDeck: 30,
     },
     {
-      difficulty: "Difícil",
-      sprite: RaichuSprite,
+      difficulty: "Hard",
+      sprite: CharizardSprite,
       cardsPerTurn: 8,
       cardsPerDeck: 40,
     },
   ];
 
+  const [currentDifficulty, setCurrentDifficulty] = useState<GameDifficulty>(
+    difficulties[0]
+  );
+
   return (
-    <div className="grid place-content-center min-h-[100dvh] ">
-      <div className="flex flex-col items-center gap-5 bg-zinc-800 bg-texture-2 bg-blend-multiply ring-8 ring-zinc-800 bg-repeat min-w-md mx-auto p-8 rounded-lg">
+    <div className="flex justify-center items-center min-h-[100dvh]  ">
+      <div className="h-[100dvh] w-fit justify-center sm:h-auto flex flex-col items-center gap-5 bg-zinc-800 bg-texture-2 bg-blend-multiply shadow-xl  shadow-zinc-950 bg-repeat mx-auto p-4 sm:p-12 rounded-lg">
         <img
-          className="mx-auto py-12 w-[30rem] brightness-125 hover:brightness-150 hover:scale-105 transition-all cursor-pointer"
+          className="mx-auto sm:pt-6 w-[30rem] brightness-125 hover:brightness-150 hover:scale-105 transition-all cursor-pointer"
           src={Logo}
           alt="PokeMem Logo"
         />
-        <button className="btn btn-layered-3d btn-layered-3d--blue text-xl">
-          Comenzar Juego
-        </button>
 
-        <div className="flex sm:flex-row flex-col gap-8 mt-12">
+        <div className="flex sm:flex-row flex-col gap-4 sm:gap-8 my-4 sm:my-8">
           {difficulties.map((diff) => (
-            <SelectDifficulty key={diff.difficulty} {...diff} />
+            <SelectDifficulty
+              key={diff.difficulty}
+              {...diff}
+              current={currentDifficulty.difficulty}
+              setCurrentDifficulty={setCurrentDifficulty}
+            />
           ))}
         </div>
+        <button className=" btn  btn-layered-3d btn-layered-3d--blue text-xl">
+          Play
+        </button>
       </div>
     </div>
   );
