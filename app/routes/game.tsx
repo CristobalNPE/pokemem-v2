063f44c25d212028b0664b48a6086b8d4712d430
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-30 03:32:18 -0300

    redirect when no difficulty set

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -1,4 +1,4 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect, type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPokemons } from "~/api/getPokemons";
 import PokemonCard from "~/components/PokemonCard";
@@ -7,6 +7,10 @@ import { gameSettings } from "~/cookies.server";
 export async function loader({ request }: LoaderFunctionArgs) {
   const cookieHeader = request.headers.get("Cookie");
   const cookie = (await gameSettings.parse(cookieHeader)) || {};
+
+  if (!cookie.hasOwnProperty("difficulty")) {
+    return redirect("/");
+  }
 
   const { pokemonListData, remaining } = await getPokemons(
     Number(cookie.cardsPerDeck)
@@ -17,17 +21,22 @@ export async function loader({ request }: LoaderFunctionArgs) {
     cardsPerDeck: cookie.cardsPerDeck,
     cardsPerTurn: cookie.cardsPerTurn,
     remaining: remaining,
-    pokemons: pokemonListData,
+    pokemons: pokemonListData, //defer this and put a loading that says something like : Shuffling your deck!
   });
 }
 
 export default function Game() {
   const gameData = useLoaderData<typeof loader>();
+  //When cards in array are all marked set to hidden? idk
+
+  //Have something like an array that holds all the gameData.pokemons 's id's, show just the correct amount
+  // according to the difficulty. When clicked, 'mark it or delete it from the array'. When all from array are marked
+  // we need to fetch again. There comes the difficulty.
 
   return (
     <>
       <h1 className="text-5xl font-bold">Game Page</h1>
-      <h1 className="text-5xl font-bold">{gameData.difficulty}</h1>
+      <h1 className="text-5xl font-bold">Difficulty: {gameData.difficulty}</h1>
       <h1 className="text-5xl font-bold">{gameData.cardsPerDeck}</h1>
       <h1 className="text-5xl font-bold">{gameData.cardsPerTurn}</h1>
 
