fc02bc72a1e112085d0ee5e6f94c404d16264bdd
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-30 01:38:15 -0300

    create basic card component

---
+++ app/components/PokemonCard.tsx
@@ -0,0 +1,24 @@
+import Tilt from "react-parallax-tilt";
+
+type Props = {
+  id: number;
+  name: string;
+  sprite: string;
+};
+
+function PokemonCard({ id, name, sprite }: Props) {
+  return (
+    <Tilt tiltReverse>
+      <article className="max-w-[15rem] bg-texture-1 bg-zinc-500  bg-blend-multiply p-2 rounded-xl flex flex-col justify-center items-center gap-2">
+        <img
+          src={sprite}
+          alt={`Sprite of ${name}`}
+          className="bg-gradient-to-r from-green-700 to-green-800 rounded-lg py-5"
+        />
+        <h1 className="text-2xl capitalize font-bold">{name}</h1>
+      </article>
+    </Tilt>
+  );
+}
+
+export default PokemonCard;
