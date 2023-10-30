6d4be18f5d59583ff56d3900ce391cfb3cc62280
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-30 20:48:58 -0300

    update fn to avoid duplicates when fetching

--- app/api/getPokemons.ts
+++ app/api/getPokemons.ts
@@ -3,11 +3,27 @@ import { createNumberArray, getRandomValuesFromArray } from "~/lib/utils";
 
 const api = new PokemonClient();
 
-export const getPokemons = async (numberOfPokemons: number) => {
-  let [numArray, remaining] = getRandomValuesFromArray(
-    numberOfPokemons,
-    createNumberArray(1017)
-  );
+export const getPokemons = async (
+  numberOfPokemons: number,
+  remainingIds: number[]
+) => {
+  let numArray,
+    remaining = [];
+
+  //We get our pokemons from an array representing the ids of all the pokemons in the database on first load.
+  if (remainingIds.length === 0) {
+    [numArray, remaining] = getRandomValuesFromArray(
+      numberOfPokemons, //6
+      createNumberArray(1017)
+    );
+  }
+  //From there, we use only the array with the remaining Ids to avoid duplicates.
+  else {
+    [numArray, remaining] = getRandomValuesFromArray(
+      numberOfPokemons,
+      remainingIds
+    );
+  }
 
   const pokemonPromises = numArray.map(async (num) => {
     const pokemonData = await api.getPokemonById(num);
