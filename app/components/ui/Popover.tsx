417ca5958157f331673fff2e50247edbec6380dc
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-30 21:29:00 -0300

    fix on small devices

--- app/components/ui/Popover.tsx
+++ app/components/ui/Popover.tsx
@@ -11,7 +11,7 @@ function Popover({ children, showPopover }: Props) {
         showPopover ? "grid" : "hidden"
       } bg-zinc-900/50 backdrop-blur-sm  absolute inset-0 z-50  place-content-center`}
     >
-      <div className="min-w-[30rem] min-h-[25rem] bg-zinc-900 rounded-lg shadow-xl p-8 flex flex-col items-center">
+      <div className="w-full sm:min-w-[30rem] min-h-[25rem] bg-zinc-900 rounded-lg shadow-xl p-8 flex flex-col items-center text-center">
         {children}
       </div>
     </div>
