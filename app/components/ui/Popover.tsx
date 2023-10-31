f4e96fb72376fb94ebb9df5bf3bd933a556b4d90
Cristobal Pulgar <cristobal.npe@gmail.com>
2023-10-30 23:47:15 -0300

    change hidden to conditional render

--- app/components/ui/Popover.tsx
+++ app/components/ui/Popover.tsx
@@ -5,16 +5,18 @@ type Props = {
 
 function Popover({ children, showPopover }: Props) {
   return (
-    <div
-      aria-hidden="true"
-      className={`${
-        showPopover ? "grid" : "hidden"
-      } bg-zinc-900/50 backdrop-blur-sm  absolute inset-0 z-50  place-content-center`}
-    >
-      <div className="w-full sm:min-w-[30rem] min-h-[25rem] bg-zinc-900 rounded-lg shadow-xl p-8 flex flex-col items-center text-center">
-        {children}
-      </div>
-    </div>
+    <>
+      {showPopover && (
+        <div
+          aria-hidden="true"
+          className={`grid bg-zinc-900/50 backdrop-blur-sm  absolute inset-0 z-50  place-content-center`}
+        >
+          <div className="w-full sm:min-w-[30rem] min-h-[25rem] bg-zinc-900 rounded-lg shadow-xl p-8 flex flex-col items-center text-center">
+            {children}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
